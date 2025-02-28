d7808fd85d814bf07f5986fbaf0b81d4484dca7d
renukana <sushmitha.renukananda.ext@nokia.com>
2025-02-28 22:17:55 +0530

    Fourth commit

---
+++ src/craco.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    babel: {
+      plugins: [
+        '@babel/plugin-proposal-private-methods',
+        '@babel/plugin-proposal-private-property-in-object',
+      ]
+    }
+  };
+  
